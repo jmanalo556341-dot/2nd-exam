865507ab78c8900e071fa8aed1175d01b05092bb
jmanalo556341-dot <j.manalo.556341@umindanao.edu.ph>
2025-10-02 17:57:10 +0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,31 +1,39 @@
-def flames_game(name1, name2):
-    # Convert names to lowercase and remove spaces
-    name1 = name1.replace(" ", "").lower()
-    name2 = name2.replace(" ", "").lower()
-
-    # Remove common letters
-    for char in name1[:]:
-        if char in name2:
-            name1 = name1.replace(char, "", 1)
-            name2 = name2.replace(char, "", 1)
-
-    total_count = len(name1) + len(name2)
-
-    if total_count == 0:
-        return "Not compatible! Single forever </3"
-
-    flames = ["Friendship", "Love", "Affection", "Marriage", "Enemy", "Sibling"]
-    idx = 0
-    while len(flames) > 1:
-        idx = (idx + total_count - 1) % len(flames)
-        flames.pop(idx)
-
-    return flames[0]
+class FlamesGame:
+    def __init__(self, name1, name2):
+        self.name1 = name1.replace(" ", "").lower()
+        self.name2 = name2.replace(" ", "").lower()
+        self.flames = ["Friendship", "Love", "Affection", "Marriage", "Enemy", "Sibling"]
+    
+    def remove_common_letters(self):
+        for char in self.name1[:]:
+            if char in self.name2:
+                self.name1 = self.name1.replace(char, "", 1)
+                self.name2 = self.name2.replace(char, "", 1)
+    
+    def calculate_result(self):
+        total_count = len(self.name1) + len(self.name2)
+        
+        if total_count == 0:
+            return "Not compatible! Single forever </3"
+        
+        idx = 0
+        while len(self.flames) > 1:
+            idx = (idx + total_count - 1) % len(self.flames)
+            self.flames.pop(idx)
+        
+        return self.flames[0]
+    
+    def get_relationship_result(self):
+        self.remove_common_letters()
+        return self.calculate_result()
 
 
 # Main Program
 if __name__ == "__main__":
     name1 = input("Enter your name: ")
     name2 = input("Enter partner's name: ")
-    result = flames_game(name1, name2)
+    
+    flames_game = FlamesGame(name1, name2)
+    result = flames_game.get_relationship_result()
+    
     print("Relationship Result:", result)
